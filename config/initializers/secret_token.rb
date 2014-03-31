190dff36ebf93390489c946f58ea61babb16e419
Vic Ramon <vic@vicramon.com>
2014-03-31 19:51:24 -0400

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberHelloWorld::Application.config.secret_key_base = 'f7e2ad999958a932ef5568b7e7f2967914b61ac56f69fe38d3692bf30480137fe286f7b467d7a81731c0ed6478c633725d4dd264ff3ebaf4e5795465294106e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmberHelloWorld::Application.config.session_store :cookie_store, key: '_ember-hello-world_session'
