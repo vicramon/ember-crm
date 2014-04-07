9b6ba79fbe13e1dd0c1841c8199a0081084d9642
Vic Ramon <vic@vicramon.com>
2014-04-06 22:17:10 -0400

    Add leads api controller

---
+++ app/controllers/api/v1/leads_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::LeadsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Lead.all
+  end
+
+  def show
+    respond_with Lead.find(params[:id])
+  end
+
+  def create
+    respond_with :api, :v1, Lead.create(lead_params)
+  end
+
+  def update
+    respond_with Lead.find(params[:id]).update_attributes(lead_params)
+  end
+
+  def destroy
+    respond_with Lead.find(params[:id]).destroy
+  end
+
+  private
+
+  def lead_params
+    params.require(:lead).permit(:first_name, :last_name, :phone, :status, :notes)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 EmberCRM::Application.routes.draw do
   root to: 'home#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :leads
+    end
+  end
 end
